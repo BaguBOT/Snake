add18c672d0fe7b5a618ee26d74ab32993ff2012
is13096 <is13096@B303-02>
2023-03-23 14:59:02 +0700

    Коментариии доделать

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -20,19 +20,17 @@ using System.Xml.Serialization;
 
 namespace snake.Games
 {
-
+    //////  Публичные значение имени и очков для списка рекордов
     public class SnakeHighscore
     {
         public string PlayerName { get; set; }
-
         public int Score { get; set; }
     }
+    ////// Публичная значения со размером змейки, кординаты и логическая проверка головы змейки 
     public class SnakePart
     {
         public UIElement UiElement { get; set; }
-
         public Point Position { get; set; }
-
         public bool IsHead { get; set; }
     }
 }
@@ -41,53 +39,61 @@ namespace Snake
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    /// 
-
     public partial class MainWindow : Window
     {
-       
+        ////// Приватное обявление использование времени системой для игры 
         private System.Windows.Threading.DispatcherTimer gameTickTimer = new System.Windows.Threading.DispatcherTimer();
+        ////// Приватное обявление рандома для дальнешего использования
         private Random rnd = new Random();
+        ////// Приватное обявление зачения еды в количесте нуливого значения
         private UIElement snakeFood = null;
-
-
+        //////  Обявление цвета для еды
         private SolidColorBrush foodBrush = Brushes.DeepPink;
-
+        ////// Размер квадрата отображающий змейку на поле
         const int SnakeSquareSize = 20;
+        ////// Стартовая длина змейки  
         const int SnakeStartLength = 4;
+        ////// Стартовая скорость змейки 
         const int SnakeStartSpeed = 300;
+        ////// максимальная скорость змейки 
         const int SnakeSpeedThreshold = 100;
-
-      
-
-
+        ////// Обявление цвета для тела и головы змейки
         private SolidColorBrush snakeBodyBrush = Brushes.MediumBlue;
         private SolidColorBrush snakeHeadBrush = Brushes.SaddleBrown;
+        ////// Обявление списка для каждой клетки змейки 
         private List<SnakePart> snakeParts = new List<SnakePart>();
-
+        ////// Обявление управляющих клавишь на клавиатуры 
         public enum SnakeDirection { Left, Right, Up, Down };
+        ////// ???
         private SnakeDirection snakeDirection = SnakeDirection.Right;
+        ////// Длина змейки 
         private int snakeLength;
+        ////// ???
         private int currentScore = 0;
         public MainWindow()
         {
+
             InitializeComponent();
+        ////// запуск игрового времени
             gameTickTimer.Tick += GameTickTimer_Tick;
+        ////// Загрузка листа с рекордами
             LoadHighscoreList();
         }
-
+        ////// Команда времени вызывающию команду движения каждый тик в ситстеме 
         private void GameTickTimer_Tick(object sender, EventArgs e)
         {
             MoveSnake();
         }
-        
 
+        ////// Команда которая запускается при открытие окна 
         private void Window_ContentRendered(object sender, EventArgs e)
         {
+            ////// Вызов разукрашивания поля
             DrawGameArea();
-            //   StartNewGame();
-
         }
+        ////// Мы обрабатываем список snakeParts в цикле,и для каждого фрагмента мы проверяем, был ли для него назначен UIElement 
+        ////// и если нет, мы создаём прямоугольник и добавляем к игровому полю не забывая сохранить ссылку на него в экземпляре SnakePart в свойстве
+        ///UiElement для позиционирования самих элементов внутри Canvas'а( игрового Поля).
         private void DrawSnake()
         {
             foreach (SnakePart snakePart in snakeParts)
@@ -106,6 +112,11 @@ namespace Snake
                 }
             }
         }
+        /// Внутри цикла while мы непрерывно создаем экземпляры элемента управления Rectangle
+        /// и добавляем их на панель Canvas (Игровое поле). Мы закрашиваем их в клетку цвет выбирайте сами,
+        /// а ширина и высота определяются нашей постоянной SnakeSquareSize поскольку мы хотим что бы ячейки были квадратными.
+        /// В каждой итерации мы используем nextX и nextY что бы определять момент перехода на следующую строку (достижение правой границы поля) а затем и момент остановки
+        /// (когда мы достигли нижней строки И правой границы поля одновременно)
         private void DrawGameArea()
         {
             bool doneDrawingBackground = false;
@@ -140,24 +151,23 @@ namespace Snake
             }
 
         }
+        ////// команда для передвижение змейки
         private void MoveSnake()
         {
-            // Remove the last part of the snake, in preparation of the new part added below  
+            ////// Удалите последнюю часть змеи, готовя новую часть, добавленную ниже
             while (snakeParts.Count >= snakeLength)
             {
                 GameArea.Children.Remove(snakeParts[0].UiElement);
                 snakeParts.RemoveAt(0);
             }
-            // Next up, we'll add a new element to the snake, which will be the (new) head  
-            // Therefore, we mark all existing parts as non-head (body) elements and then  
-            // we make sure that they use the body brush  
+            ////// Далее мы добавим к змее новый элемент, которым будет (новая) голова
+            ////// Мы помечаем все существующие детали как элементы, не относящиеся к голове (телу)   
             foreach (SnakePart snakePart in snakeParts)
             {
                 (snakePart.UiElement as Rectangle).Fill = snakeBodyBrush;
                 snakePart.IsHead = false;
             }
-
-            // Determine in which direction to expand the snake, based on the current direction  
+            ////// Определите, в каком направлении развернуть змею, основываясь на текущем направлении
             SnakePart snakeHead = snakeParts[snakeParts.Count - 1];
             double nextX = snakeHead.Position.X;
             double nextY = snakeHead.Position.Y;
@@ -176,34 +186,33 @@ namespace Snake
                     nextY += SnakeSquareSize;
                     break;
             }
-
-            // Now add the new head part to our list of snake parts...  
+            ////// Теперь добавьте новую часть головы в наш список частей змеи
             snakeParts.Add(new SnakePart()
             {
                 Position = new Point(nextX, nextY),
                 IsHead = true
             });
-           
+            ////// Вызов Создание фрагмета змейки  
             DrawSnake();
-          
+            ////// Вызов команды столкновения стенки тела змейки 
             DoCollisionCheck();
         }
+        ////// Команда для обявление новой позиции еды На игровом поле
         private Point GetNextFoodPosition()
         {
             int maxX = (int)(GameArea.ActualWidth / SnakeSquareSize);
             int maxY = (int)(GameArea.ActualHeight / SnakeSquareSize);
             int foodX = rnd.Next(0, maxX) * SnakeSquareSize;
             int foodY = rnd.Next(0, maxY) * SnakeSquareSize;
-
+            ////// Проверка чтобы не не поставить еду на саму змейку
             foreach (SnakePart snakePart in snakeParts)
             {
                 if ((snakePart.Position.X == foodX) && (snakePart.Position.Y == foodY))
                     return GetNextFoodPosition();
             }
-
             return new Point(foodX, foodY);
         }
-
+        ////// Команда для добавление еды на игровое поле 
         private void DrawSnakeFood()
         {
             Point foodPosition = GetNextFoodPosition();
@@ -217,12 +226,14 @@ namespace Snake
             Canvas.SetTop(snakeFood, foodPosition.Y);
             Canvas.SetLeft(snakeFood, foodPosition.X);
         }
+        ////// Запуск игры
         private void StartNewGame()
         {
             bdrWelcomeMessage.Visibility = Visibility.Collapsed;
             bdrHighscoreList.Visibility = Visibility.Collapsed;
             bdrEndOfGame.Visibility = Visibility.Collapsed;
-            // Remove potential dead snake parts and leftover food...
+
+        ////// Обновляем игровое поле и убираем лишьние 
             foreach (SnakePart snakeBodyPart in snakeParts)
             {
                 if (snakeBodyPart.UiElement != null)
@@ -231,25 +242,21 @@ namespace Snake
             snakeParts.Clear();
             if (snakeFood != null)
                 GameArea.Children.Remove(snakeFood);
-
-            // Reset stuff
+        ////// Сброс настроек
             currentScore = 0;
             snakeLength = SnakeStartLength;
             snakeDirection = SnakeDirection.Right;
             snakeParts.Add(new SnakePart() { Position = new Point(SnakeSquareSize * 5, SnakeSquareSize * 5) });
             gameTickTimer.Interval = TimeSpan.FromMilliseconds(SnakeStartSpeed);
-
-            // Draw the snake again and some new food...
+        //////// Создание новой змеи и еду
             DrawSnake();
             DrawSnakeFood();
-
-            // Update status
+        ////// Обновление игрового статуса 
             UpdateGameStatus();
-
-            // Go!        
+        ////// Запук внутри игрового времени      
             gameTickTimer.IsEnabled = true;
         }
-
+        ////// События с проверкой нажатие клавишь для упавления. 
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
             SnakeDirection originalSnakeDirection = snakeDirection;
@@ -279,43 +286,51 @@ namespace Snake
             if (snakeDirection != originalSnakeDirection)
                 MoveSnake();
         }
+        ////// Проверка столкновения змейки с стенкой и хвостом и едой 
         private void DoCollisionCheck()
         {
             SnakePart snakeHead = snakeParts[snakeParts.Count - 1];
-
+            ////// Столкновение с едой 
             if ((snakeHead.Position.X == Canvas.GetLeft(snakeFood)) && (snakeHead.Position.Y == Canvas.GetTop(snakeFood)))
             {
                 EatSnakeFood();
                 return;
             }
-
+            ////// Столкновение с стенкой границами поля 
             if ((snakeHead.Position.Y < 0) || (snakeHead.Position.Y >= GameArea.ActualHeight) ||
             (snakeHead.Position.X < 0) || (snakeHead.Position.X >= GameArea.ActualWidth))
             {
                 EndGame();
             }
-
+            ////// Столкновение с телом змеи
             foreach (SnakePart snakeBodyPart in snakeParts.Take(snakeParts.Count - 1))
             {
                 if ((snakeHead.Position.X == snakeBodyPart.Position.X) && (snakeHead.Position.Y == snakeBodyPart.Position.Y))
                     EndGame();
             }
         }
+        ////// Команда для поядание еды
         private void EatSnakeFood()
         {
+            ////// Увелечения змейки и очков
             snakeLength++;
             currentScore++;
+            ////// ускороние змейки 
             int timerInterval = Math.Max(SnakeSpeedThreshold, (int)gameTickTimer.Interval.TotalMilliseconds - (currentScore * 2));
             gameTickTimer.Interval = TimeSpan.FromMilliseconds(timerInterval);
             GameArea.Children.Remove(snakeFood);
+            ////// Добавление еды на поле 
             DrawSnakeFood();
+            ////// обновление игрового статуса 
             UpdateGameStatus();
         }
+        ////// Обновление значений на табло игры 
         private void UpdateGameStatus()
         {
             this.tbStatusScore.Text = currentScore.ToString();
             this.tbStatusSpeed.Text = gameTickTimer.Interval.TotalMilliseconds.ToString();
         }
+        ////// Обявление конец игры
         private void EndGame()
         {
             bool isNewHighscore = false;
@@ -336,24 +351,24 @@ namespace Snake
             }
             gameTickTimer.IsEnabled = false;
 
-           
         }
-
+        //////???
         public ObservableCollection<SnakeHighscore> HighscoreList
-        {
-            get; set;
-        } = new ObservableCollection<SnakeHighscore>();
-
+        { get; set; } = new ObservableCollection<SnakeHighscore>();
+        //////???
         private void BtnShowHighscoreList_Click(object sender, RoutedEventArgs e)
         {
             bdrWelcomeMessage.Visibility = Visibility.Collapsed;
             bdrHighscoreList.Visibility = Visibility.Visible;
         }
-         int MaxHighscoreListEntryCount = 5;
+        ////// Максимум количество для списка попыток
+        int MaxHighscoreListEntryCount = 5;
+        //////???
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
+
         private void LoadHighscoreList()
         {
             if (File.Exists("snake_list.xml"))
